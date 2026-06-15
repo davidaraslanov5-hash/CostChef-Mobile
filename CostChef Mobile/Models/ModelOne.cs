9b208f283d48734bd2a2f5e62cfcadefd8e70f32
David <Davidaraslanov5@gmail.com>
2026-06-15 12:08:28 +0500

    Создал класс ингредиентов

---
+++ CostChef Mobile/Models/ModelOne.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CostChef_Mobile.Models
+{
+    internal class ModelOne
+    {
+
+        public class Ingredient
+        {
+            public string Name { get; set; }
+            public double Price { get; set; }
+            public double UsedAmount { get; set; }
+            public double PackageSize { get; set; }
+
+            public double Cost => Price / PackageSize * UsedAmount;
+        }
+
+    }
+}
