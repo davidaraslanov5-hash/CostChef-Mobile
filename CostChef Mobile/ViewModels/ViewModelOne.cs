88dbee02a6d9683f03b8531ca6929ecf51eae156
David <Davidaraslanov5@gmail.com>
2026-06-18 00:03:02 +0500

    добавил переменные для цвета в Xaml, поиграл с дизайном

--- CostChef Mobile/ViewModels/ViewModelOne.cs
+++ CostChef Mobile/ViewModels/ViewModelOne.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using CommunityToolkit.Mvvm.ComponentModel;
-using System.Collections.Generic;
-using System.Text;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CostChef_Mobile.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
 
 namespace CostChef_Mobile.ViewModels
 {
@@ -12,20 +13,55 @@ namespace CostChef_Mobile.ViewModels
         ModelOne model = new ModelOne();
 
         [ObservableProperty]
-        public string name { get; set; }
-        public double Price { get; set; }
-        public double UsedAmount { get; set; }
-        public double PackageSize { get; set; }
-        public double Cost { get; set; }
+        public Color interactColor = Colors.BlanchedAlmond;
+        [ObservableProperty] 
+        public Color color = Colors.MintCream;
+        [ObservableProperty]
+        public Color textColor = Colors.Black;
+        [ObservableProperty]
+        public Color backgroundColor = Colors.LavenderBlush;
+
+        [ObservableProperty]
+        private string name = string.Empty;
+        [ObservableProperty]
+        public double? price;
+        [ObservableProperty]
+        public double? usedAmount;
+        [ObservableProperty]
+        public double? packageSize;
+        [ObservableProperty]
+        public double? cost;
+
+        [ObservableProperty]
+        ObservableCollection<ModelOne.Ingredient> ingredients = new ObservableCollection<ModelOne.Ingredient>();
+
+        public Command<ModelOne.Ingredient> DeleteCommand { get; set; }
 
         [RelayCommand]
-        public void CalculateCost(ModelOne.Ingredient ingredient)
+        public void CalculateCost()
         {
-            Name = ingredient.Name;
-            Price = ingredient.Price;
-            UsedAmount = ingredient.UsedAmount;
-            PackageSize = ingredient.PackageSize;
-            Cost = ingredient.Cost;
+            if (Name == string.Empty ||
+                Price == null ||
+                UsedAmount == null ||
+                PackageSize == null) ;
+            else
+            {
+                var ingredient = new ModelOne.Ingredient
+                {
+                    Name = Name,
+                    Price = Price,
+                    UsedAmount = UsedAmount,
+                    PackageSize = PackageSize
+                };
+                Cost = ingredient.Cost;
+                Ingredients.Add(ingredient);
+
+                Name = string.Empty;
+                Price = null;
+                UsedAmount = null;
+                PackageSize = null;
+            }
         }
+
     }
 }
